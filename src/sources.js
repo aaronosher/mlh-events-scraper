c15e1c63848070e87abf7010e0b099fb291cb69b
Aaron Osher <aaron@aaronosher.io>
2019-01-26 17:12:49 +0000

    Made Years dynamic

--- src/sources.js
+++ src/sources.js
@@ -1,6 +1,6 @@
 const urls = {
-    'eu': 'https://mlh.io/seasons/eu-2018/events',
-    'na': 'https://mlh.io/seasons/na-2018/events'
+    'eu': `https://mlh.io/seasons/eu-${(new Date).getFullYear()}/events`,
+    'na': `https://mlh.io/seasons/na-${(new Date).getFullYear()}/events`
 }
 
 export default urls
